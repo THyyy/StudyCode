d2d52273bcc4e0ede18d0b890c2839f3e0e10585
YuanCome <tanheyuan@sailvan.com>
2020-12-11 09:35:45 +0800

    '添加Day64打卡记录'

---
+++ src/main/java/algorithm/leetcode/everyday/Day64.java
@@ -0,0 +1,36 @@
+package algorithm.leetcode.everyday;
+
+/**
+ * 每日一题——柠檬水找零
+ *
+ * @author yuancome
+ * @date 2020/12/10
+ */
+
+public class Day64 {
+    private boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five == 0) {
+                    return false;
+                }
+                five--;
+                ten++;
+            } else {
+                if (ten > 0 && five > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}

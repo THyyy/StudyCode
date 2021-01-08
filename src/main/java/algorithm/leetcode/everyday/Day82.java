271c54907abad10c193ca3b9e591e4f27869223e
YuanCome <tanheyuan@outlook.com>
2021-01-08 23:10:21 +0800

    添加Day82打卡记录

---
+++ src/main/java/algorithm/leetcode/everyday/Day82.java
@@ -0,0 +1,27 @@
+package algorithm.leetcode.everyday;
+
+/**
+ * 每日一题——旋转数组
+ *
+ * @author yuancome
+ * @date 2021/1/8
+ */
+
+public class Day82 {
+    private void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start += 1;
+            end -= 1;
+        }
+    }
+}

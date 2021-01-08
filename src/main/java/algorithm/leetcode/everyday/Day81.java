38729797678aa1e2fd416146cd3ff193b7597478
YuanCome <tanheyuan@outlook.com>
2021-01-08 23:07:19 +0800

    添加Day81打卡记录

---
+++ src/main/java/algorithm/leetcode/everyday/Day81.java
@@ -0,0 +1,36 @@
+package algorithm.leetcode.everyday;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 每日一题——省份数量
+ *
+ * @author yuancome
+ * @date 2021/1/7
+ */
+
+public class Day81 {
+    private int findCircleNum(int[][] isConnected) {
+        int provinces = isConnected.length;
+        boolean[] visited = new boolean[provinces];
+        int circles = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < provinces; i++) {
+            if (!visited[i]) {
+                queue.offer(i);
+                while (!queue.isEmpty()) {
+                    int j = queue.poll();
+                    visited[j] = true;
+                    for (int k = 0; k < provinces; k++) {
+                        if (isConnected[j][k] == 1 && !visited[k]) {
+                            queue.offer(k);
+                        }
+                    }
+                }
+                circles++;
+            }
+        }
+        return circles;
+    }
+}
